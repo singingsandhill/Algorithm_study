62efd16b60fb42ad63102460b28bb5f40d5e6f8d
Kim jisu <64348312+singingsandhill@users.noreply.github.com>
2025-03-23 15:28:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 37.93 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,18 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder answer = new StringBuilder();
-        int len = 1;
-        for(int i=1; i<food.length; i++){
-            for (int j=0; j<food[i]/2; j++){
-                answer.append(i);
-            }
-        }
-        answer.append(0);
+        String answer = "0";
         for(int i=food.length-1; i>0; i--){
             for (int j=0; j<food[i]/2; j++){
-                answer.append(i);
+                answer= i+ answer + i;
             }
         }
-        return answer.toString();
+        return answer;
     }
 }
