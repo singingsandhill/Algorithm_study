5d49b6e573a942bb2fd0c0583a0249723eedca36
Kim jisu <64348312+singingsandhill@users.noreply.github.com>
2025-03-30 15:35:18 +0900

    [level 1] Title: 소수 만들기, Time: 2.10 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= (int)Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
